ee0606d209750a3f7c871aad23e7d72914ec47a9
Belic <Belic@DESKTOP-CQIUFP7>
2023-02-09 20:07:35 +0100

    Add project files.

---
+++ RankingApp/ClientApp/src/AppRoutes.js
@@ -0,0 +1,20 @@
+import RankItemsContainer from "./components/RankItemsContainer";
+import MovieImageArr from "./components/MovieImages.js";
+import AlbumImageArr from "./components/AlbumImages.js";
+
+const AppRoutes = [
+    {
+        index: true,
+        element: <RankItemsContainer dataType={1} imgArr={MovieImageArr} />
+    },
+    {
+        path: '/rank-movies',
+        element: <RankItemsContainer dataType={1} imgArr={MovieImageArr} />
+    },
+    {
+        path: '/rank-albums',
+        element: <RankItemsContainer dataType={2} imgArr={AlbumImageArr} />
+    }
+];
+
+export default AppRoutes;
